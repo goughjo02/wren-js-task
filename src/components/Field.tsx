eb092d516f4f103b888ca5dc2a59804bcfa43d70
goughjo02 <goughjo02@gmail.com>
2021-02-04 21:32:52 +0000

    change style of header text

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -5,16 +5,14 @@ import AddSheep from "./AddSheep";
 import SheepTable from "./SheepTable";
 import BrandRandomSheep from "./BrandRandomSheep";
 import EncourageBreeding from "./EncourageBreeding";
-import { Container, Typography } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
 
 const Field: React.FC<{ field: FieldType }> = ({ field }) => {
   const { id } = field;
   return (
     <div id={`field-${id}`}>
       <FieldProvider field={field}>
-        <Container maxWidth="sm">
-          <Typography variant="h3">A Field Of Sheep</Typography>
-        </Container>
+        <Typography variant="h4">A Field Of Sheep</Typography>
         <div className="margin-y">
           <AddSheep />
         </div>
